8482aa5382d3762de0903855133a07ea7f318263
Bailey <bbernadino96@gmail.com>
2020-01-01 09:48:28 -0700

    Updates

---
+++ precache-manifest.4b535fc5a9ef90fec6982042a33bdafc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40014ade843c232b32a7acbe478a1c8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e45ba0095cd12cccd2f3",
+    "url": "/monsters-rolodex/static/css/main.6a11bb26.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e45ba0095cd12cccd2f3",
+    "url": "/monsters-rolodex/static/js/main.904629ca.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
